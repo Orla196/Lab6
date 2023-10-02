cf12898b951dba77d55054445c9017bbdcd81336
unknown <g00397035@atu.ie>
2023-10-02 15:38:47 +0100

    Lab 2 Complete With Comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/*This is how you comment */}
+        {/*Dark nav bar added*/}
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Creat</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        {/*Added a route to show content, header and footer on different pages*/}
+        <Routes>
+          <Route path='/' element={<Content></Content>}></Route>
+          <Route path='/read' element={<Header></Header>}></Route>
+          <Route path='/create' element={<Footer></Footer>}></Route>
+
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
